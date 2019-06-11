d3187d2c9334655abb8639a039e5ea54177fccbc
jazz-code <ren.meta@gmail.com>
2019-06-11 16:42:51 -0400

    closure MVP

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,15 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function closureOuter() {
+    var x = "Hi";
+    function closureInner() {
+        console.log(x);
+    }
+    console.log('test', x);
+    return closureInner;   
+}
+closureOuter();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
